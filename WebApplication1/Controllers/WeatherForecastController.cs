d16cd165109f41b5b6a7f8bf4c64b5ac8387506d
Aman Bhardwaj <bhardwajaman252@gmail.com>
2026-07-29 23:48:44 +0530

    add new prop default name

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -18,7 +18,8 @@ namespace WebApplication1.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+               
             })
             .ToArray();
         }

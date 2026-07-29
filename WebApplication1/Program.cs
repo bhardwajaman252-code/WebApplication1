085658a41aa5858e2162a507a793e17cc526ae4d
Aman Bhardwaj <bhardwajaman252@gmail.com>
2026-07-30 00:11:59 +0530

    swagger dev

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
